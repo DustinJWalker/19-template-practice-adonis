be52372c4c204c89807b628907d48445774bdaff
Dustin Walker <dwtimp@gmail.com>
2016-11-06 14:34:36 -0600

    setting up home view

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -15,6 +15,13 @@
 | Route.resource('user', 'UserController')
 */
 
-const Route = use('Route')
+const Route = use('Route');
+const fetch = require('node-fetch');
 
-Route.on('/').render('welcome')
+// Route.on('/').render('welcome')
+Route.get('/', function* (request, response){
+
+  const apiUrl = yield fetch('http://json-data.herokuapp.com/forms');
+  const formInputs = yield apiUrl.json();
+  yield.response.sendView('form', {formInputs});
+})
